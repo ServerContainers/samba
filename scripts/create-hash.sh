4b8ec09a3171b00ae7316f22c89457fba07c67a5
Marvin Winkler <m@rvin.at>
2020-12-22 16:23:42 +0100

    fix with smb.conf

--- scripts/create-hash.sh
+++ scripts/create-hash.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# replace original smb config
+grep '# This is the main Samba configuration file.' /etc/samba/smb.conf 2>/dev/null >/dev/null && cp /container/config/samba/smb.conf /etc/samba/smb.conf
+
 echo -n '>> Enter username: ' 1>&2
 read USERNAME
 echo -n '>> New password: ' 1>&2
