494c7c584738f928bb2f1fa8da8e32c83a42f9d8
Marvin <MarvAmBass@users.noreply.github.com>
2021-03-09 08:58:45 +0100

    fixed healthcheck

--- scripts/docker-healthcheck.sh
+++ scripts/docker-healthcheck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -f "/external/avahi/not-mounted" ]; then
-  [[ $(ps aux | grep '[s]mbd -d\|[r]unsvdir' | wc -l) -ge '3' ]]
+  [[ $(ps aux | grep '[s]mbd -d\|[r]unsvdir' | wc -l) -ge '2' ]]
   exit $?
 else
   [[ $(ps aux | grep '[s]mbd -d\|[a]vahi-daemon\|[r]unsvdir' | wc -l) -ge '3' ]]
