2168a4fe7299efc80a3f0cba12e9dc6d221be1c5
Marvin Winkler <m@rvin.at>
2021-12-30 22:21:44 +0100

    small fixes

--- build.sh
+++ build.sh
@@ -1,23 +1,23 @@
-#!/bin/sh
+#!/bin/sh -x
 
 IMG="servercontainers/samba"
 
 PLATFORM="linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6"
 
-rm variants.tar variants/ 2>/dev/null >/dev/null
+rm -rf variants.tar variants/ 2>/dev/null >/dev/null
 
 if [ -z ${SAMBA_VERSION+x} ] || [ -z ${SAMBA_VERSION+x} ]; then
-  docker-compose build --pull --no-cache
+  docker-compose build -q --pull --no-cache
   export SAMBA_VERSION=$(docker run --rm -ti "$IMG" smbd -V | tail -n1 | cut -d' ' -f2 | tr -d '\r')
   export ALPINE_VERSION=$(docker run --rm -ti "$IMG" cat /etc/alpine-release | tail -n1 | tr -d '\r')
 fi
 
-docker buildx build --pull --no-cache --platform "$PLATFORM" -t "$IMG:a$ALPINE_VERSION-s$SAMBA_VERSION" --push .
+docker buildx build -q --pull --no-cache --platform "$PLATFORM" -t "$IMG:a$ALPINE_VERSION-s$SAMBA_VERSION" --push .
 
-echo "$@" | grep "release" 2>/dev/null >/dev/null && echo ">> releasing new latest" && docker buildx build --platform "$PLATFORM" -t "$IMG:latest" --push .
+echo "$@" | grep "release" 2>/dev/null >/dev/null && echo ">> releasing new latest" && docker buildx build -q --pull --platform "$PLATFORM" -t "$IMG:latest" --push .
 
 # make sure this is only executed in main script
-pwd | grep "variant" && exit 0 
+echo "$@" | grep "variant" && exit 0 
 
 
 tar cf variants.tar --exclude .git/ --exclude variants.tar .
@@ -35,7 +35,7 @@ rm -rf config/runit/wsdd2
 sed -i.bak 's/:[a]/:smbd-only-a/g' build.sh && rm build.sh.bak
 sed -i.bak 's/:[l]atest/:smbd-only-latest/g' build.sh && rm build.sh.bak
 
-./build.sh "$@"
+./build.sh "variant" "$@"
 
 cd ../../
 
@@ -49,7 +49,7 @@ rm -rf config/runit/wsdd2
 sed -i.bak 's/:[a]/:smbd-avahi-a/g' build.sh && rm build.sh.bak
 sed -i.bak 's/:[l]atest/:smbd-avahi-latest/g' build.sh && rm build.sh.bak
 
-./build.sh "$@"
+./build.sh "variant" "$@"
 
 cd ../../
 
@@ -63,6 +63,6 @@ rm -rf config/avahi config/runit/avahi
 sed -i.bak 's/:[a]/:smbd-wsdd2-a/g' build.sh && rm build.sh.bak
 sed -i.bak 's/:[l]atest/:smbd-wsdd2-latest/g' build.sh && rm build.sh.bak
 
-./build.sh "$@"
+./build.sh "variant" "$@"
 
 cd ../../
