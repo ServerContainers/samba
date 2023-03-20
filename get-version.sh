bf56c1b08fdd287b19fb77eb99c1ca442892ceb2
Marvin Winkler <m@rvin.at>
2023-03-20 09:17:08 +0100

    use versioning tag as docker tag

--- get-version.sh
+++ get-version.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-if [ -z ${SAMBA_VERSION+x} ] || [ -z ${ALPINE_VERSION+x} ]; then
-  IMG=$(docker build -q --pull --no-cache -t 'get-version' .)
-  export SAMBA_VERSION=$(docker run --rm -ti "$IMG" apk list 2>/dev/null | grep '\[installed\]' | grep "samba-[0-9]" | cut -d " " -f1 | sed 's/samba-//g' | tr -d '\r')
-  export ALPINE_VERSION=$(docker run --rm -ti "$IMG" cat /etc/alpine-release | tail -n1 | tr -d '\r')
-fi
+export IMG=$(docker build -q --pull --no-cache -t 'get-version' .)
+export SAMBA_VERSION=$(docker run --rm -ti "$IMG" apk list 2>/dev/null | grep '\[installed\]' | grep "samba-[0-9]" | cut -d " " -f1 | sed 's/samba-//g' | tr -d '\r')
+export ALPINE_VERSION=$(docker run --rm -ti "$IMG" cat /etc/alpine-release | tail -n1 | tr -d '\r')
+
+[ -z "$ALPINE_VERSION" ] && exit 1
+
 echo "a$ALPINE_VERSION-s$SAMBA_VERSION"
 
