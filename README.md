d39e702244b9203d0410a5a91f41da1cf9aaa9bd
Marvin Winkler <m@rvin.at>
2024-03-11 13:49:13 +0100

    overwrite builds if commit within last hour

--- README.md
+++ README.md
@@ -51,6 +51,7 @@ _all of those variants are automatically build and generated in one go_
 
 * 2024-03-11
     * patch `host-name` in `/etc/avahi/avahi-daemon.conf` if `AVAHI_NAME` env is set
+    * fixed build - overwrite current version if there was a commit within last hour
 * 2024-01-08
     * multiline config for shares available
         * optional
--- build.sh
+++ build.sh
@@ -8,9 +8,17 @@ rm -rf variants.tar variants/ 2>/dev/null >/dev/null
 
 TAG=$(./get-version.sh)
 
-if echo "$@" | grep -v "force" 2>/dev/null >/dev/null; then
-  echo "check if image was already build and pushed - skip check on release version"
-  echo "$@" | grep -v "release" && docker pull "$IMG:$TAG" 2>/dev/null >/dev/null && echo "image already build" && exit 1
+if echo "$@" | grep -v "force" 2>/dev/null >/dev/null; then  
+  # if there was a commit within the last hour, rebuild the container - even if it's already build
+  ONE_HOUR_IN_SECONDS=3600
+  EPOCH_SINCE_LAST_PUSH=$(git log -1 --format=%cd --date=iso | xargs -I {} date -d "{}" +%s || git log -1 --format=%cd --date=iso | xargs -I {} date -jf '%Y-%m-%d %H:%M:%S %z' "{}" +%s)
+  SECONDS_SINCE_LAST_PUSH=$(expr $(date +%s) - $EPOCH_SINCE_LAST_PUSH)
+  if [ "$SECONDS_SINCE_LAST_PUSH" -gt "$ONE_HOUR_IN_SECONDS" ]; then
+    echo "check if image was already build and pushed - skip check on release version"
+    echo "$@" | grep -v "release" && docker pull "$IMG:$TAG" 2>/dev/null >/dev/null && echo "image already build" && exit 1
+  else
+    echo "commit within the last hour, we skip the version check and try to overwrite current build"
+  fi
 fi
 
 docker buildx build -q --pull --no-cache --platform "$PLATFORM" -t "$IMG:$TAG" --push .
