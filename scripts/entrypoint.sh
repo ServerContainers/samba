160767fcee535fa8e0fe9b6c59c2b9d32c508753
Marvin <MarvAmBass@users.noreply.github.com>
2024-03-11 12:45:13 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -251,6 +251,7 @@ if [ ! -f "$INITALIZED" ]; then
   done
 
   [ ! -z ${AVAHI_NAME+x} ] && echo ">> ZEROCONF: custom avahi samba.service name: $AVAHI_NAME" && sed -i 's/%h/'"$AVAHI_NAME"'/g' /etc/avahi/services/samba.service
+  [ ! -z ${AVAHI_NAME+x} ] && echo ">> ZEROCONF: custom avahi avahi-daemon.conf host-name: $AVAHI_NAME" && sed -i "s/#host-name=foo/host-name=${AVAHI_NAME}/" /etc/avahi/avahi-daemon.conf
 
   echo ">> ZEROCONF: samba.service file"
   echo "############################### START ####################################"
