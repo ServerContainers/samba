88a501e3c20ca077fb688d57943b54c51c4ca359
Marvin Winkler <m@rvin.at>
2024-06-13 18:03:06 +0200

    fixed #133 variants healthcheck

--- generate-variants.sh
+++ generate-variants.sh
@@ -12,6 +12,9 @@ mv Dockerfile.new Dockerfile
 rm -rf config/runit/avahi
 rm -rf config/runit/wsdd2
 
+sed -i.bak '/avahi/d' ./scripts/docker-healthcheck.sh && rm ./scripts/docker-healthcheck.sh.sh.bak
+sed -i.bak '/WSD/d' ./scripts/docker-healthcheck.sh && rm ./scripts/docker-healthcheck.sh.sh.bak
+
 sed -i.bak 's/:$TAG" --push/:smbd-only-$TAG" --push/g' build.sh && rm build.sh.bak
 sed -i.bak 's/:[l]atest/:smbd-only-latest/g' build.sh && rm build.sh.bak
 
@@ -24,6 +27,8 @@ cat Dockerfile | grep -v wsdd2 > Dockerfile.new
 mv Dockerfile.new Dockerfile
 rm -rf config/runit/wsdd2
 
+sed -i.bak '/WSD/d' ./scripts/docker-healthcheck.sh && rm ./scripts/docker-healthcheck.sh.sh.bak
+
 sed -i.bak 's/:$TAG" --push/:smbd-avahi-$TAG" --push/g' build.sh && rm build.sh.bak
 sed -i.bak 's/:[l]atest/:smbd-avahi-latest/g' build.sh && rm build.sh.bak
 
@@ -35,6 +40,8 @@ tar xf ../../variants.tar
 cat Dockerfile | grep -v avahi > Dockerfile.new
 mv Dockerfile.new Dockerfile
 rm -rf config/runit/avahi
+
+sed -i.bak '/avahi/d' ./scripts/docker-healthcheck.sh && rm ./scripts/docker-healthcheck.sh.sh.bak
 
 sed -i.bak 's/:$TAG" --push/:smbd-wsdd2-$TAG" --push/g' build.sh && rm build.sh.bak
 sed -i.bak 's/:[l]atest/:smbd-wsdd2-latest/g' build.sh && rm build.sh.bak
